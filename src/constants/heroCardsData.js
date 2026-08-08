9c25be3fbd1cc8093d438c49d4078f352992f03c
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-08-08 15:39:24 +0300

    edit the pics problem

--- src/constants/heroCardsData.js
+++ src/constants/heroCardsData.js
@@ -1,8 +1,8 @@
 const picsData = [
-    "/public/bounce1.avif",
-    "/public/bounce2.avif",
-    "/public/bounce3.avif",
-    "/public/bounce4.avif",
-    "/public/bounce5.avif",
+    "/bounce1.avif",
+    "/bounce2.avif",
+    "/bounce3.avif",
+    "/bounce4.avif",
+    "/bounce5.avif",
 ]
 export default picsData;
